a61fcc8ac7498c24ed520928149634b8602c523c
HoneyTruda <jyotirmayg97@gmail.com>
2026-03-16 21:26:40 +0530

    Application Properties,User,Activity,ActivityType

---
+++ src/main/java/com/project/fitness/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.project.fitness.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    SWIMMING,
+    CYCLING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
