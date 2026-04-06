ad0d02807b07ef1e700d7ee840c6a8a6fbe7ae14
HoneyTruda <jyotirmayg97@gmail.com>
2026-04-06 19:23:12 +0530

    Implementing ResponseEntity in AuthController and added UserResponse

--- src/main/java/com/project/fitness/controller/AuthController.java
+++ src/main/java/com/project/fitness/controller/AuthController.java
@@ -2,14 +2,10 @@ package com.project.fitness.controller;
 
 import com.project.fitness.dto.RegisterRequest;
 import com.project.fitness.model.User;
-import com.project.fitness.repository.UserRepository;
 import com.project.fitness.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,7 +15,7 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public User register(@RequestBody RegisterRequest registerRequest){
-        return userService.register(registerRequest);
+    public ResponseEntity<User> register(@RequestBody RegisterRequest registerRequest){
+        return ResponseEntity.ok(userService.register(registerRequest));
     }
 }
