a9023182ebfaaa210553e7c9fd1aa803bf05cfd5
HoneyTruda <jyotirmayg97@gmail.com>
2026-04-09 12:30:38 +0530

    Implementing Activity Details

--- src/main/java/com/project/fitness/model/Activity.java
+++ src/main/java/com/project/fitness/model/Activity.java
@@ -6,7 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
@@ -18,6 +20,7 @@ import java.util.Map;
 @Data // It's the combination of @Getter,@Setter annotation, it's part of lombok
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Activity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -37,7 +40,10 @@ public class Activity {
     private Integer duration;
     private Integer caloriesBurned;
     private LocalDateTime startTime;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "activity",cascade = CascadeType.ALL,orphanRemoval = true)
